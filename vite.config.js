fa36d9a078c0c61a518e0d1b3bffef7cddb02e30
bbelen111 <187245515+bbelen111@users.noreply.github.com>
2025-11-22 18:33:24 +0800

    refactor: add server configuration to ensure dev server is accessible on the LAN

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Ensure dev server is accessible on the LAN (same as running `vite --host`)
+  server: {
+    host: true,
+  },
+});
