bb3c7e5a11285b11643601c3db8c9ce0c96540e4
bbelen111 <187245515+bbelen111@users.noreply.github.com>
2026-01-21 11:17:48 +0800

    fix: enhance scrollbar handling and disable overscroll in WebView

--- android/app/src/main/java/com/energymap/tracker/MainActivity.java
+++ android/app/src/main/java/com/energymap/tracker/MainActivity.java
@@ -1,5 +1,20 @@
 package com.energymap.tracker;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the WebView and disable scrollbars
+        WebView webView = this.getBridge().getWebView();
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+
+        // Disable the "bounce" glow effect
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+    }
+}
